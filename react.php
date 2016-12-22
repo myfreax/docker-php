62dab28ce1563e7b66d41fe4300d632c5fb8c250
Freax <huangyanxiong2013@gmail.com>
2016-12-22 17:56:38 +0800

    react php example

---
+++ react.php
@@ -0,0 +1,14 @@
+<?php
+require './vendor/autoload.php';
+$loop = new React\EventLoop\StreamSelectLoop();
+$socket = new React\Socket\Server($loop);
+$http = new React\Http\Server($socket, $loop);
+$i = 0;
+$http->on('request', function ($request, $response) use (&$i) {
+    $i++;
+    $response->writeHead();
+    $response->end("Hello World!\n");
+});
+
+$socket->listen(8080,'0.0.0.0');
+$loop->run();
